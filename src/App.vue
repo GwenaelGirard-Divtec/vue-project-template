31508448be43aef0fd7a8c928564258b7f81c53f
Gwenael Girard <gwenael.girard@divtec.ch>
2022-11-11 09:40:40 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <router-view/>
 </template>
 
+<script>
+export default {
+    name: 'App',
+    components: {}
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+    color: $black;
+    font-family: "Roboto", sans-serif;
+    box-sizing: border-box;
+    margin: 0 !important;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+    background-color: $white;
+    padding: 16px;
 }
 </style>
